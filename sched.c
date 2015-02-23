bc836ec03bb161f2c1a4d5e318a45079e4e22c07
Echallier Nicolas <echallier@a12p4.fil.univ-lille1.fr>
2015-02-23 10:45:24 +0100

    implementation fonctions klock() et kunlock() et ajout dans sched.c pour proteger les ressources du kernel"

--- sched.c
+++ sched.c
@@ -1,15 +1,43 @@
 #include "sched.h"
 
+
+/* manages locks */
+/* basically, each time we use mega_ctx, we make sure that no other core can change it */
+static void klock() {
+  int i = 0;
+  /* we check that the lock is free */
+  if(_in(CORE_LOCK) == 1) {
+    /* we take the lock */
+    _out(CORE_LOCK, 0x2);
+  }
+  /* if it not free, we wait a bit and retry */
+  else {
+    while(i < 0xFFFF) {
+      i++;
+    }
+    klock();
+  }
+
+}
+
+static void kunlock() {
+  _out(CORE_UNLOCK, 0x1);
+}
+
+
+
+
 void print_ctx(struct ctx_s *ctx){
   printf("|> ");
   printf(BOLDWHITE"%s with state %d\n"RESET,ctx->ctx_name,ctx->ctx_state);
 }
 
 void print_pile_ctx(){
-  irq_disable();
   int currentCor = _in(CORE_ID);
   unsigned int i;
   struct ctx_s *ctx;
+  irq_disable();
+  klock();
   assert(mega_ctx[currentCor].ring_head != NULL);
   ctx = mega_ctx[currentCor].ring_head;
 
@@ -20,15 +48,21 @@ void print_pile_ctx(){
     print_ctx(ctx);
     ctx = ctx->ctx_next;
   }
+  kunlock();
   printf(BLUE"\n=============================\n"RESET);
   irq_enable();
 }
 
 
 int init_ctx(struct ctx_s *ctx, int stack_size, func_t f,struct parameters * args,char *name){
-
+  irq_disable();
+  klock();
   ctx->ctx_stack = (char*) calloc(stack_size,sizeof(char));
-  if ( ctx->ctx_stack == NULL) return 1;
+  if ( ctx->ctx_stack == NULL) {
+    irq_enable();
+    kunlock();
+    return 1;
+  }
   int corToInit = randRob%CORE_NCORE;
 
   printf(BOLDGREEN"[create_ctx] corToInit = %d\n"RESET, corToInit);
@@ -49,21 +83,29 @@ int init_ctx(struct ctx_s *ctx, int stack_size, func_t f,struct parameters * arg
 
   if(DEBUG)
     printf(BOLDBLUE"\n%d ) creating ctx %s on \n"RESET,mega_ctx[corToInit].nb_ctx,name);
-
+  
+  irq_enable();
+  kunlock();
   return 0;
 }
 
 
 int create_ctx(int size, func_t f, struct parameters * args,char *name){
-
-
   irq_disable();
+  klock();
   struct ctx_s* new_ctx = (struct ctx_s*) calloc(1,sizeof(struct ctx_s));
   int corToInit = randRob++%CORE_NCORE;
 
   assert(new_ctx);
 
-  if(init_ctx(new_ctx, size, f, args ,name)){ /* error */ return 1; }
+  irq_enable();
+  kunlock();
+  if(init_ctx(new_ctx, size, f, args ,name)){ 
+    /* error */ 
+    return 1; 
+  }
+  irq_disable();
+  klock();
   /* if this is the first ctx that is initialised, we initialise the ring_head */
   if(!mega_ctx[corToInit].ring_head){
     mega_ctx[corToInit].ring_head = new_ctx;
@@ -73,43 +115,12 @@ int create_ctx(int size, func_t f, struct parameters * args,char *name){
     new_ctx->ctx_next = mega_ctx[corToInit].ring_head->ctx_next;
     mega_ctx[corToInit].ring_head->ctx_next = new_ctx;
   }
+  kunlock();
   irq_enable();
-
   return 0;
 }
 
 
-
-
-
-
-void del_ctx(struct ctx_s *ctx){
-  irq_disable();
-  int currentCor = _in(CORE_ID);
-  assert(ctx != NULL);
-  if(DEBUG)
-    printf(RED"Deleting : "RESET);
-
-  if(DEBUG)
-    print_ctx(ctx);
-
-  if(DEBUG)
-    printf(RED"ADDRESS : %p \n"RESET,ctx);
-
-  /* si il n'y a qu'un seul context, on libere l'espace memoire de la stack et le pointeur de la structure */
-  /* if(ctx == ctx->ctx_next){ */
-  /*   free(ctx->ctx_stack); */
-  /*   free(ctx); */
-  /* } else { */
-  /*   struct ctx_s *suivant = ctx->ctx_next; */
-  /*   struct ctx_s *precedent = ctx; */
-  /*   while(precedent->ctx_next != ctx ) */
-  /*     precedent = precedent->ctx_next; */
-  /*   precedent->ctx_next = suivant; */
-  /*   ctx = NULL; */
-  /* } */
-
-}
 
 void start(){
   printf("Entering in start for schedule");
@@ -148,6 +159,7 @@ void switch_to_ctx(struct ctx_s *new_ctx){
     mega_ctx[currentCor].current_ctx->ctx_state = CTX_END;
     yield();
   }
+  kunlock();
   irq_enable();
 }
 
@@ -156,6 +168,8 @@ void switch_to_ctx(struct ctx_s *new_ctx){
 void yield(){
   int currentCor = _in(CORE_ID);
   irq_disable();
+  klock();
+  /* we reinitialise the timer's interuption */
   _out(TIMER_ALARM,TIMER);  /* alarm at next tick (at 0xFFFFFFFF) */
   if(DEBUG){
     printf("\n !!!!!!!! CPT : %d!!!!!!!\n",cpt++);
@@ -168,12 +182,16 @@ void yield(){
       print_ctx(mega_ctx[currentCor].ring_head);
     printf(GREEN"\n======================\n"RESET);
     print_pile_ctx();
+
   }
 
   /* if the current context of the thread has no jobs to do */
   if(!mega_ctx[currentCor].current_ctx){
     mega_ctx[currentCor].current_ctx = mega_ctx[currentCor].ring_head;
     mega_ctx[currentCor].current_ctx->ctx_next = mega_ctx[currentCor].ring_head;
+    irq_enable();
+    kunlock();
+
     switch_to_ctx(mega_ctx[currentCor].current_ctx);
   } else {
     /* we set the new context we are going to look at */
@@ -203,6 +221,8 @@ void yield(){
 	break;
       }
     }
+    irq_enable();
+    kunlock();
 
     switch_to_ctx(ctx);
   }
@@ -242,65 +262,4 @@ void wait_disque(){
   yield();
 }
 
-
-
-/* void sem_init(struct sem_s *sem, unsigned int val, char* name){ */
-/*   printf(BOLDGREEN"[init semaphore disque]"RESET GREEN"\n"RESET); */
-/*   assert(sem); */
-/*   sem->init_cpt = sem->sem_cpt = val; */
-/*   sem->sem_head = NULL; */
-/*   sem->sem_name = name; */
-/*   assert(sem); */
-/* } */
-
-/* void sem_up(struct sem_s *sem){ */
-
-/*   irq_disable(); */
-/*   if(DEBUG){ */
-/*     printf(BOLDMAGENTA"\n[Tentative de rendu de semaphre] %s avec comme etat : %d\n"RESET,ctx_disque->ctx_name,sem->sem_cpt); */
-/*   } */
-/*   sem->sem_cpt++; */
-/*   if(sem->sem_cpt > 0){ */
-/*     struct ctx_s *ctx_tmp = sem -> sem_head->ctx_next; */
-/*     sem->sem_head->ctx_state = CTX_EXQ; */
-/*     sem->sem_head->ctx_next = ring_head; */
-/*     current_ctx = sem->sem_head; */
-/*     sem->sem_head = ctx_tmp; */
-/*     yield(); */
-/*   } */
-/*   if(DEBUG){ */
-/*     printf(BOLDMAGENTA"\n[SEMAPHORE DISQUE RENDU PAR] %s avec comme nouvel etat : %d\n"RESET,ctx_disque->ctx_name,sem->sem_cpt); */
-/*   } */
-
-/*   irq_enable(); */
-
-
-/* } */
-
-
-/* void sem_down(struct sem_s *sem){ */
-/*   irq_disable(); */
-/*   if(DEBUG) */
-/*     printf(BOLDMAGENTA"\n[Tentative de prise de semaphre] %s avec comme etat : %d\n"RESET,current_ctx->ctx_name,sem->sem_cpt); */
-
-/*   if(sem->sem_cpt == 0){ */
-/*     printf("FREEZE"); */
-/*     current_ctx->ctx_state = CTX_STP; */
-/*     if(!sem->sem_head){ */
-/*       current_ctx->ctx_sem_next = sem->sem_last; */
-/*       sem->sem_head = current_ctx; */
-/*     }else{ */
-/*       current_ctx->ctx_sem_next = sem->sem_head; */
-/*       sem->sem_last->ctx_next = current_ctx; */
-/*       sem->sem_last = current_ctx; */
-/*     } */
-/*     yield(); */
-/*   } */
-/*   sem->sem_cpt--; */
-/*   if(DEBUG) */
-/*     printf(BOLDMAGENTA"\n[SEMAPHORE DISQUE PRIS PAR] %s avec comme nouvel etat : %d\n"RESET,current_ctx->ctx_name,sem->sem_cpt); */
-
-/*   irq_enable(); */
-/*   yield(); */
-/* } */
 
